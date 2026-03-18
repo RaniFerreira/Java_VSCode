65852f76282f8d04f69401d00d941a6330461585
RaniFerreira <franielly535@gmail.com>
2026-03-18 17:17:01 -0300

    add secao3 simple exercise

---
+++ secao3/Tabuada.java
@@ -0,0 +1,34 @@
+package secao3;
+
+import java.util.Scanner;
+
+public class Tabuada{
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o número que deseja ver a tabuada ");
+
+        int num = scan.nextInt();
+
+        System.out.println(num + " x 1 = " + (num * 1));
+        System.out.println(num + " x 2 = " + (num * 2));
+        System.out.println(num + " x 3 = " + (num * 3));
+        System.out.println(num + " x 4 = " + (num * 4));
+        System.out.println(num + " x 5 = " + (num * 5));
+        System.out.println(num + " x 6 = " + (num * 6));
+        System.out.println(num + " x 7 = " + (num * 7));
+        System.out.println(num + " x 8 = " + (num * 8));
+        System.out.println(num + " x 9 = " + (num * 9));
+        System.out.println(num + " x 10 = " + (num * 10));
+
+        scan.close();
+
+
+
+
+    }
+}
+
+
+
